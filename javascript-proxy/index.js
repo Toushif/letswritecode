5480da943705c5aec40568e01bf85da20139686e
Toushif <toushif.haq@gmail.com>
2021-11-21 02:55:37 +0530

    js proxy index.js added Reflect

--- javascript-proxy/index.js
+++ javascript-proxy/index.js
@@ -3,15 +3,17 @@ let bears = { grizzly: true }
 let grizzlyCount = 0
 
 const proxyBears = new Proxy(bears, {
-  get: function (target, prop) {
+  get: function (target, prop, receiver) {
     if (prop === 'grizzly') grizzlyCount++
-    return target[prop]
+    return Reflect.get(target, prop, receiver) //instead of returning target[prop] we can use Reflect like this
+    // return target[prop]
   },
-  set: function (target, prop, value) {
+  set: function (target, prop, value, receiver) {
     if (['grizzly', 'brown', 'polar'].indexOf(prop) === -1) {
       throw new Error('THAT IS TOTALLY NOT A BEAR!')
     }
-    target[prop] = value
+    return Reflect.set(target, prop, value, receiver)
+    // target[prop] = value
   },
   deleteProperty: function (target, prop) {
     console.log(`You have deleted ${prop}`)
@@ -21,14 +23,16 @@ const proxyBears = new Proxy(bears, {
 
 //proxyBears.aardvark = true
 proxyBears.polar = true
+proxyBears.brown = false
 //delete proxyBears.polar
 //console.log(proxyBears.polar)
+console.log(bears)
 
-// proxyBears.grizzly
-// proxyBears.grizzly
-// proxyBears.grizzly
-// proxyBears.grizzly
-// console.log(grizzlyCount)
+proxyBears.grizzly
+proxyBears.grizzly
+proxyBears.grizzly
+proxyBears.grizzly
+console.log(grizzlyCount)
 
 function growl() {
   return 'grrr'
